b6a03b279e806e1caff7c2d2ab91c252dacfec7a
1paleking1 <1paleking1@gmail.com>
2024-09-03 21:16:19 +0100

    added updated raspberry pi code for camera usage and CPU temps. Also removed the service account key which leaked. The old leaked one is now deleted

--- RPI/main.py
+++ RPI/main.py
@@ -1,9 +1,10 @@
 import gpiod
 import firebase_admin
-from firebase_admin import credentials, db
+from firebase_admin import credentials, db, storage
 import sys
 from time import sleep
 import schedule
+from picamera2 import Picamera2, Preview
 
 print("imports worked")
 
@@ -15,28 +16,22 @@ led_line.request(consumer="LED", type=gpiod.LINE_REQ_DIR_OUT)
 app_listener_initialised = False
 ping_listener_initialised = False
 shutdown_listener_initialised = False
+camera_listener_initialised = False
 
 serviceAccountKey = {
-  "type": "service_account",
-  "project_id": "plant-app-d11b6",
-  "private_key_id": "7fc8ac5ceb5d4225064e5e670ee08afe376ee474",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCzha5RQf3VoNTb\ncCsLjo0/jVLgSYJ7DpU3ABcS04mwyzIlCgYPTg3JDXsyN8uTzgZLE394Se7G9nYD\naud195rxG7yp0gQ9Wa2JON/z1hT7qd1ndksLYIhk3eDsqEnbAp+fk0kw1gSUWvOm\nKf5IAS27Noc0gz8RA3xy8Di42fZ4U15GuH9h1U/nGJUB5KWuY+3Hx+pisTpwxuNo\n6KmTJD0aB3G1NYHzeAIiCTe1yThCW4Lmu5UtT2uXZmpP1IPsH5AIgJpyE/BMlXYd\nDS9ApI37tWdnMMg0jYC05Q0L/0sj+zodQsekYZJctlbJLe9GB2lE65VjFxqdXC14\nJplMClw1AgMBAAECggEAEjEpj0v17lxYPa2B+y+Usjw+ygJBAfniFpsAe5Sqtxr8\nR6MappnlDTr7wUTEsUN3QUvmRrkNRKXMoz+F2Z9AGQ9TwMzKx7HLA2UbUsEeyrr1\nuAqVg79iSwBuDuVl6Jb9OIVLLTL+707BMpU+Uqsl6HsX2/Mx5KY7CgW8ZcUWtC4p\nIOYjcHYy+iV/XcS0T+Ub7opAZMIxtinl6do/YxI7rDE6I4zzK2DPJUrHejiRkHfC\n7jErTkJuVWDsgCnoUWzWP3CpANe6wplnfxeQ3fwqyGc7Q6u66sP1Ey3jIP24UQce\n7sir4BLC9Ga+eCjqodE7k0rg0bWaHKkWQHoBZH9dUQKBgQDb1ntC47EVwcrNxvbE\nDVk1pxadrEpsgdu9dch+hYTlw0cy53Px9i30LLVgPi7oAlLNBsh9P3JmHGMsGQrr\nO0gAQNOEV/IX7WZWc9jCa4CxyLYhB84BDYw0saoQxUPpmVVeqJs8Rme2nOGhj2Xg\nkLW+4aiU1nrvB/CZ2byTVoV3kQKBgQDRDXsyd3exe8GIxhwk7aDKC9nTIBgN2/fu\n55WSrx6zV/0C8HHoBJ0wlEde+Cot04fywHtWeW7K9EzmunK7SJOAFKhvGOY87ZvX\neOctuDgJ7rXuzqvPuDdf9JWCwIbjMcLRMjvqR5C4cZUhMw82qa7bY7xX+uLkO5an\n/4EH/gcwZQKBgQCQxrd2Xa0Jvw5oZles1X2ir99dB1IcE4k7RI49hID5L5l8FKFu\n/8lTvP3V3B3X/W+6RnVSxq34QUmuiO9/6xKH2Cl0RtbQmy+CT9i4cUPX0W7gYdtx\nJhLnwPlXAhKvjKOEK1XQZC15Pef6Fg3pzmNiygnTu0xKjtgM+jAOeDR9gQKBgGqQ\n9LxFKj7PV1jhpHcuq0hTwjZdpeVqvHiMlTKxEnVZ5SAacfZUY8zfyYIf/2Bc9HBK\nrpTmUhQjdRAo+1N9K7Oe1D3N85Abj9XHTBGCVw65IQUbVXoN7ix8AbslaZ1nQGhj\nsyc6VAux2tORs6+pz/kGXnx66C5wMdxx4tSwKfoBAoGAWVR6AZEcTCpyfroM5yTB\nUSQBO3IY6VjHr0eApPxBlol0Zv2cNL397l0qttz3vzHGUQTJOqG080yXstbLTl33\nZEhhSfnZUj+dwHeUBTEgknLQUFJwU+N5GKW2F4GADd1PoxnY8VDQ22YM86wIrFrr\nQcDFaCMYWYkdGqgdR7wtyg0=\n-----END PRIVATE KEY-----\n",
-  "client_email": "firebase-adminsdk-y0wib@plant-app-d11b6.iam.gserviceaccount.com",
-  "client_id": "111187030105577891820",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-y0wib%40plant-app-d11b6.iam.gserviceaccount.com",
-  "universe_domain": "googleapis.com"
-}
+  
+} # SECRET! ADD YOUR OWN SERVICE ACCOUNT KEY HERE
+
 
 cred = credentials.Certificate(serviceAccountKey)
 firebase_admin.initialize_app(
     cred,
     {
+        'storageBucket': 'plant-app-d11b6.appspot.com',
         'databaseURL': 'https://plant-app-d11b6-default-rtdb.europe-west1.firebasedatabase.app/'
     }
 )
+
 
 print("firebase setup worked")
 
@@ -65,12 +60,10 @@ def ReceiveAppSignal(event):
 
             days = db.reference('/days').get()
             print("STARTING TIMED WATERING: ", days)
-            
+
             for day in days:
                 getattr(schedule.every(), day.lower()).at("12:00").do(PerformTimedWatering, seconds_to_water=3)
 
-
-            
 
     db.reference("/pi_signal").set(event.data)
 
@@ -87,9 +80,13 @@ def ReceiveAppPing(event):
 
     db.reference("/response_pi_pong").set(event.data)
 
-def ReceiveShutdownSignal(event):
+    with open("/sys/class/thermal/thermal_zone0/temp", "r") as file:
+        temp_raw = file.read()
+        temp = f"{str(temp_raw)[0:2]}.{str(temp_raw)[3:]}"
+        print(temp)
+        db.reference("/pi_cpu_response").set(float(temp.strip()))
 
-    print("attempting to shutdown pi")
+def ReceiveShutdownSignal(event):
 
     global shutdown_listener_initialised
 
@@ -101,6 +98,33 @@ def ReceiveShutdownSignal(event):
         print("SHUTTING DOWN")
         db.reference("/shutdown_pi_signal").set(True)
         sys.exit()
+
+
+def ReceiveCameraSignal(event):
+
+    # ADD CODE HERE TO TAKE PHOTO AND SAVE IMAGE
+
+    global camera_listener_initialised
+
+    if not camera_listener_initialised:
+        camera_listener_initialised = True
+        return
+
+    picam2 = Picamera2()
+    camera_config = picam2.create_preview_configuration()
+    picam2.configure(camera_config)
+    picam2.start()
+    sleep(2)
+    picam2.capture_file("latest_image.jpg")
+    picam2.close()
+
+    bucket = storage.bucket()
+    filename = "latest_image.jpg"
+
+    blob = bucket.blob(filename)
+    blob.upload_from_filename(filename)
+
+    print("new picture uploaded")
 
 
 def PerformTimedWatering(seconds_to_water):
@@ -119,6 +143,7 @@ try:
     app_signal_subscription = db.reference('/app_signal').listen(ReceiveAppSignal)
     ping_pong_subscription = db.reference('/response_app_ping').listen(ReceiveAppPing)
     shutdown_signal = db.reference('/shutdown_app_signal').listen(ReceiveShutdownSignal)
+    camera_signal = db.reference('/camera_app_signal').listen(ReceiveCameraSignal)
 
     while True:
         schedule.run_pending()
